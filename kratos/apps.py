fb5cdc4f0eedf8f7966037428d7c1cd9f6574a18
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-10-01 20:55:25 +0300

    [Fix] Celery: Move celery.py to apps.py

--- kratos/apps.py
+++ kratos/apps.py
@@ -1,4 +1,12 @@
+import os
+
 from suit.apps import DjangoSuitConfig
+
+from celery import Celery
+
+celery_app = Celery('proj')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
 
 
 class SuitConfig(DjangoSuitConfig):
--- kratos/tasks.py
+++ kratos/tasks.py
@@ -2,7 +2,7 @@ import json
 
 from django.core.mail import send_mail
 
-from .celery import app
+from .apps import celery_app as app
 
 
 @app.task
